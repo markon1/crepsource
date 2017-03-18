aaf60885f8ed1059352a10517b6c10423fa3450d
markon1 <zmstefan@gmail.com>
2017-03-18 22:08:23 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,103 @@
+var express = require('express'),
+    mysql = require('mysql'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+var port = 3001;
+
+var pool = mysql.createPool({
+    connectionLimit: 20,
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "words"
+});
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(function (req, res, next) { // allow all origins
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.post('/add', function (req, res) {
+    var word = req.body.word;
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            console.error("Could not get connection from pool");
+            console.error(err);
+        } else {
+            connection.query("INSERT INTO words (word, number) VALUES (?, 1) ON DUPLICATE KEY UPDATE number = number+1;", word, function (err, results) {
+                if (err) {
+                    console.error("Error while adding word: "+word +" to DB");
+                    console.error(err);
+                } else {
+                    res.send("ok");
+                    connection.release();
+                }
+            });
+        }
+    });
+});
+
+app.post('/remove', function (req, res) {
+    var word = req.body.word;
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            console.error("Could not get connection from pool");
+            console.error(err);
+        } else {
+            connection.query("UPDATE words SET number = number - 1 WHERE word = ? AND number > 0", word, function (err, results) {
+                if (err) {
+                    console.error("Error while removing word: "+word +" from DB");
+                    console.error(err);
+                } else {
+                    res.send("ok");
+                    connection.release();
+                }
+            });
+        }
+    });
+});
+
+app.get('/getMost', function (req, res) {
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            console.error("Could not get connection from pool");
+            console.error(err);
+        } else {
+            connection.query("SELECT word FROM words WHERE number > 0 ORDER BY number DESC LIMIT 7", function (err, results) {
+                if (err) {
+                    console.error("Error while getting most from DB");
+                    console.error(err);
+                } else {
+                    res.send(results);
+                    connection.release();
+                }
+            });
+        }
+    });
+});
+
+app.get('/getWeekly', function (req, res) {
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            console.error("Could not get connection from pool");
+            console.error(err);
+        } else {
+            connection.query("SELECT word FROM weekly", function (err, results) {
+                if (err) {
+                    console.error("Error while getting weekly from DB");
+                    console.error(err);
+                } else {
+                    res.send(results);
+                    connection.release();
+                }
+            });
+        }
+    });
+});
+
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
